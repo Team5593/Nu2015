9f649b7ead79f8c99c3813a919bd85ef4e31d9bc
AVirgona <a.p.virgona@gmail.com>
2015-02-24 16:19:29 +1100

    error fixing

--- FRC_Main/src/Robot.cpp
+++ FRC_Main/src/Robot.cpp
@@ -1,4 +1,5 @@
 #include "WPILib.h"
+//#include "time.h"
 
 #define JoyDownButton 2
 #define JoyUpButton 3
@@ -15,31 +16,33 @@
 
 class Robot: public IterativeRobot
 {
+	Timer timer;
+	RobotDrive myRobot;				//robot drive system
+	Joystick Xcont; 				//Main Driver control system
+	Joystick stick;					//Co-Driver controller system
+	Jaguar m_liftMotor;				//Motor Controller
+	Jaguar m_grabMotor;				//Motor Controller
+	LiveWindow *lw;					//Live Window
+	Encoder lift_encoder;			//Encoder for Lifting
+	DigitalInput limitSwitch;		//Limit switch for lifting
+	BuiltInAccelerometer accel;		//Built In Accelerometer
 
-	RobotDrive myRobot;			// robot drive system
-	Joystick Xcont; 			// only joystick
-	Joystick stick;
-	Talon m_liftMotor;
-	Talon m_grabMotor;
-	LiveWindow *lw;
-	Encoder lift_encoder;
-	DigitalInput di;
-
-	DigitalInput limitSwitch;
-	BuiltInAccelerometer accel;
-
+	double timeStart;
+	double secCount;
+	double speed = 0;
+	double distance = 0;
+	double acc = 0;
 
 public:
 	Robot() :
-		myRobot(0, 1),			// these must be initialized in the same order
-		Xcont(0),				// as they are declared above.
+		myRobot(0, 1),				//these must be initialized in the same order
+		Xcont(0),					//as they are declared above.
 		stick(1),
 		m_liftMotor(2),
 		m_grabMotor(3),
 		lw(NULL),
 		lift_encoder(0,1),
-		limitSwitch(2),
-		accel(0)
+		limitSwitch(2)
 	{
 		myRobot.SetExpiration(0.1);
 	}
@@ -48,94 +51,134 @@ private:
 	void RobotInit()
 	{
 		lw = LiveWindow::GetInstance();
-
-di.
-
-		accel.SetRange(Accelerometer::Range::kRange_8G);	// Set accel range to 8G's
-
 		SmartDashboard::init();
 	}
 
 	void AutonomousInit()
 	{
+		timeStart = timer.GetFPGATimestamp();
 
-		/*Starting behind tote
 
-		 Grab tote, waiting until limit switch
+		/*
+		Starting behind tote
 
-		 Lift tote to level 2, detected by encoder
+		Grab tote, waiting until limit switch
 
-		 !!!Get to Auto zone!!! Difficult due to no encoders atm.
-		 Will probably have to be timed - Means a lot of testing trial and error(Especially on the day)
-		 Alternative:
-		 Use
+		Lift tote to level 2, detected by encoder
 
-		 Stay in the auto zone
-		 */
+		!!!Get to Auto zone!!! Difficult due to no encoders atm.
+		Will probably have to be timed - Means a lot of testing trial and error(Especially on the day)
+	 	Alternative:
+		Use
+
+		Stay in the auto zone
+		*/
 	}
 
 	void AutonomousPeriodic()
 	{
+		/*
+		double timeStart = clock_t();
 
+
+		double time_end = clock_t();
+
+		double time_elapsed = timeStart - time_end;
+		this->
+		sleep(0.002-time_elapsed);
+
+		SmartDashboard::PutNumber("Time Elapsed", time_elapsed);
+		*/
+
+		double timeNow = timer.GetFPGATimestamp();
+		double time_elapsed = timeNow - timeStart;
+
+
+		if(time_elapsed > 0.002) {
+			//Deadzones and setting Accelerometer value
+			if (accel.GetY() < 0.03 or accel.GetY() < -0.03){
+				acc = 0;
+			}
+			else{
+				acc = accel.GetY();
+			}
+
+			speed	 = speed+(acc*0.002);
+			distance = distance+(speed*0.002);
+
+			timeStart = timeNow;
+			secCount++;
+			SmartDashboard::PutNumber("Distance", distance);
+			SmartDashboard::PutNumber("Get Raw Y", accel.GetY());
+			SmartDashboard::PutNumber("Deadzone Calculated", acc);
+			SmartDashboard::PutNumber("Time Elapsed", secCount);
+
+		}
 	}
 
 	void TeleopInit()
 	{
 		SmartDashboard::init();
+
+
 		// Controller Output
-		SmartDashboard::PutNumber("Axis Left", Xcont.GetRawAxis(1));	// Print Left Axis
-		SmartDashboard::PutNumber("Axis Right", Xcont.GetRawAxis(5));	// Print Right Axis
+		SmartDashboard::PutNumber("Axis Left", Xcont.GetRawAxis(1));	//Print Left Axis
+		SmartDashboard::PutNumber("Axis Right", Xcont.GetRawAxis(5));	//Print Right Axis
 		// Motor output
-		SmartDashboard::PutNumber("LeftOut", 0);						// Print leftOut
-		SmartDashboard::PutNumber("RightOut", 0);						// Print rightOut
+		SmartDashboard::PutNumber("LeftOut", 0);						//Print leftOut
+		SmartDashboard::PutNumber("RightOut", 0);						//Print rightOut
 		// Encoder
-		SmartDashboard::PutNumber("EncoderDirection", 0);				// Print encoder direction
-		SmartDashboard::PutNumber("EncoderRotations", 0);				// Print encoder rotations
+		SmartDashboard::PutNumber("EncoderDirection", 0);				//Print encoder direction
+		SmartDashboard::PutNumber("EncoderRotations", 0);				//Print encoder rotations
 		// Accelerometer
-		SmartDashboard::PutNumber("Get X", 0);
-		SmartDashboard::PutNumber("Get Y", 0);
-		SmartDashboard::PutNumber("Get Z", 0);
+		SmartDashboard::PutNumber("Get X", 0);							//Accelerometer X
+		SmartDashboard::PutNumber("Get Y", 0);							//Accelerometer Y
+		SmartDashboard::PutNumber("Get Z", 0);							//Accelerometer Z
 
 		SmartDashboard::PutBoolean("Switch", 0);
 	}
 
 	void TeleopPeriodic()
 	{
+		/* ---------- LIMIT SWITCH ----------*/
+		SmartDashboard::PutBoolean("Switch", limitSwitch.Get());
+
+
 		/* ---------- VARIABLES ---------- */
-		double leftOut 	= 0;								// Left Output
-		double rightOut = 0;								// Right Output
-		double leftIn 	= -Xcont.GetRawAxis(1);				// leftIn (left Input) is set to XboxController (Axis 5 right thumbstick)
-		double rightIn 	= -Xcont.GetRawAxis(5);				// rightIn (right Input) is set to XboxController (Axis 5 right thumbstick)
-		float speed 	= 1-(Xcont.GetRawAxis(3)/2);		// Speed is inverted, then set to XboxController (Axis 3 right trigger), then divided by 2
+		double leftOut 	= 0;									//Left Output
+		double rightOut = 0;									//Right Output
+		double leftIn 	= -Xcont.GetRawAxis(1);					//leftIn (left Input) is set to XboxController (Axis 5 right thumbstick)
+		double rightIn 	= -Xcont.GetRawAxis(5);					//rightIn (right Input) is set to XboxController (Axis 5 right thumbstick)
+		float speed 	= 1-(Xcont.GetRawAxis(3)/2);			//Speed is inverted, then set to XboxController (Axis 3 right trigger), then divided by 2
 		bool manualLift  = true;
 		int setHeight = 0;
 		double heights[] = {1000.0,2000.0,3000.0,4000.0};		//Change these for levels of totes
-																//no math just magic numbers!
+																//no math just magic numbers! Do you beleive in magic?
 
 
-		/* ---------- DEADZONES ---------- */				// Input below 0.08 (8%) is set to 0.00
-		if (leftIn 	< 0.08 && leftIn > -0.08){				// L Thumb-Stick
+		/* ---------- DEADZONES ---------- */					//Input below 0.08 (8%) is set to 0.00
+		if (leftIn 	< 0.08 && leftIn > -0.08){					//L Thumb-Stick
 			leftIn 	= 0;
 		}
-		if (rightIn < 0.08 && rightIn > -0.08){				// R Thumb-Stick
+		if (rightIn < 0.08 && rightIn > -0.08){					//R Thumb-Stick
 			rightIn = 0;
 		}
 
 		/* ---------- DRIVE OUTPUT ---------- */
-		leftOut 	= leftIn*speed;							// L Speed
-		rightOut 	= rightIn*speed;						// R Speed
+		leftOut 	= leftIn*speed;								//L Speed
+		rightOut 	= rightIn*speed;							//R Speed
 
-		myRobot.TankDrive(leftOut,rightOut); 				// Drive, accounts for speed.
+		myRobot.TankDrive(leftOut,rightOut); 					//Drive, accounts for speed.
 
 		/* --------- ENCODER VALUES ---------- */
-		bool direction = lift_encoder.GetDirection();
+		//bool direction = lift_encoder.GetDirection(); //Is this even needed?
 		int EncoderDir;
 		double EncoderRot;
 
 		EncoderRot = lift_encoder.GetDistance();
 
 		/* ---- MANUAL GRABBING OUTPUT ---- */
-		if (stick.GetRawButton(JoyDownButton)){			//Left Joystick Button
+		if (stick.GetRawButton(JoyLeftButton)){			//Left Joystick Button
 			m_grabMotor.Set(1);							//Grabs
 		}
 		else if (stick.GetRawButton(JoyRightButton)){	//Right Joystick Button
@@ -146,49 +189,48 @@ di.
 		}
 
 		/* ---- AUTO LIFT BUTTON CHECKING --- */
-
-		if (stick.GetRawButton(Lvl1Button)){
+		if (stick.GetRawButton(Lvl1Button)){			//Set height to 1 box
 			setHeight = 1;
 			manualLift = false;
 		}
-		if (stick.GetRawButton(Lvl2Button)){
+		if (stick.GetRawButton(Lvl2Button)){			//Set height to 2 box
 			setHeight = 2;
 			manualLift = false;
 		}
-		if (stick.GetRawButton(Lvl3Button)){
+		if (stick.GetRawButton(Lvl3Button)){			//Set height to 3 box
 			setHeight = 3;
 			manualLift = false;
 		}
-		if (stick.GetRawButton(Lvl4Button)){
+		if (stick.GetRawButton(Lvl4Button)){			//Set height to 4 box
 			setHeight = 4;
 			manualLift = false;
 		}
-		if (stick.GetRawButton(ResetEncoderButton)){
+		if (stick.GetRawButton(ResetEncoderButton)){	//Reset Height
 			lift_encoder.Reset();
 		}
 
 		/* ------ MANUAL LIFT OUTPUT ------ */
-		if (stick.GetRawButton(JoyUpButton)){			//Joystick Up Button
+		if (stick.GetRawButton(JoyUpButton)){			//Joystick Up Button "Going Up"
 			m_liftMotor.Set(1);
 			manualLift = true;
 		}
 
-		else if (stick.GetRawButton(JoyDownButton)){	//Joystick Down Button
+		else if (stick.GetRawButton(JoyDownButton)){	//Joystick Down Button "Going Down"
 			m_liftMotor.Set(-1);
 			manualLift = true;
-		} else {
+		} else{
 			m_liftMotor.Set(0);
 		}
 
 		/* ---------- AUTO LIFT ---------- */
 		if (!manualLift){
 			if (EncoderRot < (heights[setHeight-1] * 0.99) ){
-				SmartDashboard::PutNumber("EncoderTarget",heights[setHeight-1] * 0.99 ); //Going Up
+				SmartDashboard::PutNumber("EncoderTarget",heights[setHeight-1] * 0.99 ); 	//Going Up. *Elevator Music*
 				m_liftMotor.Set(1);
 				EncoderDir = 1;
 			}
 			else if (EncoderRot > (heights[setHeight-1] * 1.01) ){
-				SmartDashboard::PutNumber("EncoderTarget",heights[setHeight-1] * 1.01 ); //Going Down
+				SmartDashboard::PutNumber("EncoderTarget",heights[setHeight-1] * 1.01 ); 	//Going Down
 				m_liftMotor.Set(-1);
 				EncoderDir = -1;
 			}
@@ -199,21 +241,24 @@ di.
 			}
 		}
 
+
 		/* ---------- SMART DASHBOARD ---------- */
-		SmartDashboard::PutNumber("LeftOut", leftOut);					// Print leftOut
-		SmartDashboard::PutNumber("RightOut", rightOut);				// Print rightOut
-		SmartDashboard::PutNumber("Axis Left", Xcont.GetRawAxis(1));	// Print Left Axis
-		SmartDashboard::PutNumber("Axis Right", Xcont.GetRawAxis(5));	// Print Right Axis
-		SmartDashboard::PutNumber("EncoderDirection", EncoderDir);		// Print encoder direction
-		SmartDashboard::PutNumber("EncoderRotations", EncoderRot);		// Print encoder distance
-		SmartDashboard::PutNumber("manualLift", manualLift);			// Print manualLift
-		SmartDashboard::PutNumber("setHeight", setHeight);				// Print setHeight
+		//Motor and Controller Outputs
+
+
+		SmartDashboard::PutNumber("LeftOut", leftOut);					//Print leftOut
+		SmartDashboard::PutNumber("RightOut", rightOut);				//Print rightOut
+		SmartDashboard::PutNumber("Axis Left", Xcont.GetRawAxis(1));	//Print Left Axis
+		SmartDashboard::PutNumber("Axis Right", Xcont.GetRawAxis(5));	//Print Right Axis
+		// Lift Encoder
+		SmartDashboard::PutNumber("EncoderDirection", EncoderDir);		//Print encoder direction
+		SmartDashboard::PutNumber("EncoderRotations", EncoderRot);		//Print encoder distance
+		SmartDashboard::PutNumber("manualLift", manualLift);			//Print manualLift
+		SmartDashboard::PutNumber("setHeight", setHeight);				//Print setHeight
 		// Accelerometer Raw Input
-		SmartDashboard::PutNumber("Get X", accel.GetX());
-		SmartDashboard::PutNumber("Get Y", accel.GetY());
-		SmartDashboard::PutNumber("Get Z", accel.GetZ());
-		bool di = limitSwitch.CheckAnalogInput(2);
-		SmartDashboard::PutBoolean("Switch", di);
+		SmartDashboard::PutNumber("Get Raw X", accel.GetX());
+		SmartDashboard::PutNumber("Get Raw Y", accel.GetY());
+		SmartDashboard::PutNumber("Get Raw Z", accel.GetZ());
 	}
 
 	void TestPeriodic()
